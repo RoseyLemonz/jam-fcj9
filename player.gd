70d11cefd0611eab0ce8813015fda900008a81ff
RoseyLemonz <97610612+RoseyLemonz@users.noreply.github.com>
2025-10-28 13:41:14 +0200

    Add skybox, better player defs and temp blocks

--- player.gd
+++ player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody3D
 
-# players speed (m/s)
-@export var speed = 10
-# air accel (also m/s)
+# players speed
+@export var def_speed = 4
+@export var speed = def_speed
+# air accel
 @export var fall_accel = 50
+# vertical impulse
+@export var jump_impulse = 14
+# player scale
+@export var pl_scale = 1
+
 
 var target_velocity = Vector3.ZERO
 
@@ -23,6 +29,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 		
+		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		# this affects the rotation of the player, so we rotate them back
@@ -32,6 +39,24 @@ func _physics_process(delta):
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed 
 	
+	# jumping
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		target_velocity.y = jump_impulse
+		
+	# crouching
+	if Input.is_action_pressed("crouch"):
+		scale.y = pl_scale * 0.6
+		speed = def_speed / 2
+	
+	# crouch jump height
+	if Input.is_action_pressed("crouch") and Input.is_action_pressed("jump"):
+		scale.y = pl_scale * 0.8
+	
+	# set scale to normal when not crouching
+	if not Input.is_action_pressed("crouch"):
+		scale.y = pl_scale
+		speed = def_speed
+	
 	# air velocity
 	if not is_on_floor():	# detect if we arent on the floor
 		target_velocity.y = target_velocity.y - (fall_accel * delta)
@@ -39,8 +64,3 @@ func _physics_process(delta):
 	# actually move character
 	velocity = target_velocity
 	move_and_slide()
-	
-	
-func _camera_control():
-	
-	$CameraPivot.position = $Pivot.position
